897bf811c6437b555148eeac63db41e1ea87d94b
Jerome Bridge <jeromebridge@gmail.com>
2015-08-29 15:13:07 -0400

    Installing Eclipse with a couple of plugins

--- README.md
+++ README.md
@@ -4,9 +4,30 @@ Vagrant configuraiton for Yet Another Admin System development environment.
 ## Tools
 * Java Decompiler
 * Eclipse (with plugins)
+    **Plugins**
+    * OSGi Integration Plugin: http://repo1.maven.org/maven2/.m2e/connectors/m2eclipse-tycho/0.7.0/N/0.7.0.201309291400/
+    * AspectJ Development Tools: http://download.eclipse.org/tools/ajdt/44/dev/update/
+    * AspectJ M2E Configurer: http://dist.springsource.org/release/AJDT/configurator/
+    * TestNG Plugin: http://beust.com/eclipse
+    * Java Decompiler Plugin: Special instructions
+    * buildhelper Maven Connector
+    * m2e-apt Maven Connector
+    
+    **Preferences**
+    
 * Git
 * Docker
 * Get Repositories?
 * Squirrel SQL (setup for database)
 * <del>Maven</del>
 * <del>JDK(s)</del>
+
+
+## Debugging
+````
+cd /vagrant/puppet
+sudo puppet apply manifests/default.pp --debug --modulepath=modules
+````
+
+## Todo
+1. Setup Maven `settings.xml`
--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -1,7 +1,47 @@
+# What is an OS agnostic workstation.
+Exec {
+  path => ["/bin/", "/sbin/", "/usr/bin/", "/usr/sbin/"] }
+
 class { "jdk_oracle":
   version  => "7",
   version_update => "67",
 }
+
 class { "maven::maven":
-  version => "3.0.4"
+  version => "3.3.2"
+}
+
+class { 'eclipse':
+    method          => 'download',
+    release_name    => 'luna',
+    service_release => 'R'
+  } ->
+  # Puppet editor for eclipse
+  # class { 'eclipse::plugin::geppetto': method => 'p2_director', } # ->
+  # Eclipse egit
+  #class { 'eclipse::plugin::egit': method => 'p2_director', } ->
+  # Edit shell scripts from within eclipse
+  class { 'eclipse::plugin::shelled': method => 'p2_director', } ->
+  # # yaml editor for eclipse
+  #class { 'eclipse::plugin::yedit': method => 'p2_director', } ->
+  # # Markdown editor for eclipse
+  class { 'eclipse::plugin::markdown': method => 'p2_director', } # ->
+  # Ruby Editor for eclipse
+  #class { 'eclipse::plugin::dltk::ruby': } ->
+  #class { 'eclipse::plugin::colortheme': method => 'p2_director', }
+  
+  
+exec { 'fix-eclipse-memory1':
+  command => 'sed -i "s/^256m/1024m/" /opt/eclipse/eclipse.ini',
+  unless  => "grep -- '1024m' /opt/eclipse/eclipse.ini"
+}
+
+exec { 'fix-eclipse-memory2':
+  command => 'sed -i "s/^-XX:MaxPermSize=256m/-XX:MaxPermSize=1024m/" /opt/eclipse/eclipse.ini',
+  unless  => "grep -- '-XX:MaxPermSize=1024m' /opt/eclipse/eclipse.ini"
+}
+
+exec { 'fix-eclipse-memory3':
+  command => 'sed -i "s/^-Xmx512m/-Xmx1024m/" /opt/eclipse/eclipse.ini',
+  unless  => "grep -- '-Xmx1024m' /opt/eclipse/eclipse.ini"
 }
